eee7dd9a4b816c005acce49d26285f0ef6d535a0
Ranjit Vishwakarma <vishwakarmaranjit8109@gmail.com>
2023-10-29 10:24:20 +0530

    Initial commit

---
+++ Amazon/Amazonapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def base1page(request):
+    return render(request, 'base1.html')
+
+def base2page(request):
+    return render(request, 'base2.html')
+
+def first_page(request):
+    return render(request, 'first.html')
